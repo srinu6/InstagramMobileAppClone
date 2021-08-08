2664524d6ad462e5bb394b77edb96ace98dd5f06
Srinuvasarao Maripi <srinumaripi.nitsri@gmail.com>
2021-08-08 18:12:29 +0530

    UI

--- src/components/Post/components/Footer/index.js
+++ src/components/Post/components/Footer/index.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import ADIcon from 'react-native-vector-icons/AntDesign'
-import FontistoIcon from 'react-native-vector-icons/Fontisto'
-import IoniconsIcon from 'react-native-vector-icons/Ionicons'
-import FAIcon from 'react-native-vector-icons/FontAwesome'
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import ADIcon from 'react-native-vector-icons/AntDesign';
+import FontistoIcon from 'react-native-vector-icons/Fontisto';
+import IoniconsIcon from 'react-native-vector-icons/Ionicons';
+import FAIcon from 'react-native-vector-icons/FontAwesome';
 
 const Footer = ({likesCount, caption, postedAt}) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(likesCount);
+
+  const likeOnPress = () => {
+    const amount = isLiked ? -1 : 1;
+    setLikeCount(likeCount + amount);
+    setIsLiked(!isLiked);
+  };
+
   return (
     <View style={styles.container}>
-        <View style={styles.iconsStyle}>
-            <View style={styles.iconStyle}>
-                <ADIcon name="hearto" size={25} color={"#545454"} />
-                <FontistoIcon name="comment" size={23} color={"#545454"} />
-                <IoniconsIcon name="paper-plane-outline" size={27} color={"#545454"} />
-            </View>
-           <View style={styles.right}>
-            <FAIcon name="bookmark-o" size={25} /> 
-           </View>
-            
+      <View style={styles.iconsStyle}>
+        <View style={styles.iconStyle}>
+          <TouchableOpacity onPress={likeOnPress}>
+            {isLiked ? (
+              <ADIcon name="heart" size={25} color={'#e73252'} />
+            ) : (
+              <ADIcon name="hearto" size={25} color={'#545454'} />
+            )}
+          </TouchableOpacity>
+
+          <FontistoIcon name="comment" size={23} color={'#545454'} />
+          <IoniconsIcon
+            name="paper-plane-outline"
+            size={27}
+            color={'#545454'}
+          />
         </View>
-      <Text style={styles.likes}>{likesCount} Likes</Text>
+        <View style={styles.right}>
+          <FAIcon name="bookmark-o" size={25} />
+        </View>
+      </View>
+      <Text style={styles.likes}>{likeCount} Likes</Text>
       <Text style={styles.captionStyle}>{caption}</Text>
       <Text style={styles.postedStyle}>{postedAt}</Text>
     </View>
@@ -34,15 +53,15 @@ const styles = StyleSheet.create({
   },
   iconStyle: {
     flexDirection: 'row',
-    width:120,
-    justifyContent:'space-between'
+    width: 120,
+    justifyContent: 'space-between',
   },
   iconsStyle: {
     flexDirection: 'row',
-    justifyContent:'space-between'
+    justifyContent: 'space-between',
   },
   right: {
-    marginRight:8
+    marginRight: 8,
   },
   likes: {
     fontWeight: 'bold',
--- src/components/Post/components/Header/index.js
+++ src/components/Post/components/Header/index.js
@@ -3,18 +3,16 @@ import {View, Text, StyleSheet} from 'react-native';
 import ProfilePicture from '../../../ProfilePicture';
 import Icon from 'react-native-vector-icons/Entypo';
 
-
 const Header = ({imageUri, name}) => {
   return (
     <View style={styles.container}>
-    <View style={styles.left}>
-       <ProfilePicture uri={imageUri} size={40} />
-       <Text style={styles.testDesign}>{name}</Text>   
-    </View> 
-    <View style={styles.right} >
-       <Icon name="dots-three-vertical" size={16} />
-    </View>
-     
+      <View style={styles.left}>
+        <ProfilePicture uri={imageUri} size={40} />
+        <Text style={styles.testDesign}>{name}</Text>
+      </View>
+      <View style={styles.right}>
+        <Icon name="dots-three-vertical" size={16} />
+      </View>
     </View>
   );
 };
@@ -26,14 +24,14 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     fontWeight: 'bold',
-    justifyContent:'space-between'
+    justifyContent: 'space-between',
   },
   left: {
     flexDirection: 'row',
     alignItems: 'center',
   },
   right: {
-    marginRight: 15
+    marginRight: 15,
   },
   testDesign: {
     fontWeight: 'bold',
